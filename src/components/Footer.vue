8bb92f4b05a0792ab2b1a8495b74bad4485dc052
JulianHelperstorfer <72748301+JulianHelperstorfer@users.noreply.github.com>
2022-11-18 10:31:30 +0100

    18.11.2022 | created components

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template></template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {},
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>The easiest way to create QR Code</h1>
+  <h2>Generate a QR Code and download it!</h2>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {},
+};
+</script>
+
+<style scoped></style>
